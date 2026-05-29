28c9d967954914b0fc5aef65310b03db9e05c418
jiun <vonreal20@gmail.com>
2026-05-29 15:31:44 +0900

    feat: Embedding __init__ 구현

--- src/embeddings.py
+++ src/embeddings.py
@@ -27,7 +27,9 @@ class InputEmbedding(nn.Module):
         self.emb_dim = emb_dim
         self.context_length = context_length
         # TODO: token_embedding, position_embedding, dropout을 정의하세요.
-        raise NotImplementedError("InputEmbedding.__init__을 구현하세요.")
+        self.token_embedding = nn.Embedding(vocab_size, self.emb_dim)
+        self.position_embedding = nn.Embedding(self.context_length, self.emb_dim)
+        self.dropout = nn.Dropout(drop_rate)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
